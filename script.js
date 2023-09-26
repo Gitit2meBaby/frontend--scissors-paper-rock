77208af721dfed6c9035a115ca58ba06d766207e
Gitit2meBaby <thisworldofdans@gmail.com>
2023-09-26 20:56:19 +0700

    game working, counter not finished, issue now with removing old content, now trying to resolve it by passing the 'title' into the divs to be appended. No responsive design

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const gameDisplay = document.querySelector('.app')
-
 const resultWaitDisplay = document.querySelector('.result');
 const finalResultDisplay = document.querySelector('.result-display')
+const resultText = document.querySelector('#resultText')
+const playAgainBtn = document.querySelector('#playAgain')
+const yourChoice = document.querySelector('.your-choice')
+const computerChoiceDisplay = document.querySelector('.computer-choice');
+
+let userChoiceData;
+let computerChoiceData;
+let counter = 0
 
 
 // template for choice made
-const yourChoiceTemplate = (data) => `
+const yourChoiceTemplate = (data, title) => `
+<p>${title}</p>
 <div class="border ${data} upsize upsize-border">
         <div class="circle upsize">
           <img class="upsize" src="images/icon-${data}.svg" alt="hand symbol for ${data}">
@@ -17,23 +25,27 @@ const choices = document.querySelectorAll('.choices')
 
 choices.forEach((choice) => {
     choice.addEventListener('click', () => {
-        console.log('clicked')
         data = choice.getAttribute('data-choice')
-        appendChoice(data)
+        let title = 'YOU PICKED'
+        appendChoice(data, title)
         randomChoice()
+        userChoiceData = data
+
+        compareChoices(userChoiceData, computerChoiceData)
     })
 })
 
 // passing the choice to a new div and changing screens
 function appendChoice(data) {
     const userChoice = document.createElement('div');
+    userChoice.classList.add('choice')
     userChoice.innerHTML = yourChoiceTemplate(data)
-
-    const yourChoice = document.querySelector('.your-choice')
     yourChoice.appendChild(userChoice);
 
     gameDisplay.classList.add('hidden')
     resultWaitDisplay.classList.remove('hidden')
+    console.log(data)
+    return data
 }
 
 
@@ -44,25 +56,88 @@ const max = 3;
 
 function randomChoice() {
     const randomInteger = Math.floor(Math.random() * (max - min)) + min;
-    console.log(randomInteger);
 
     const computerChoiceValue = choiceArray[randomInteger];
-    console.log(computerChoiceValue)
+    let data = computerChoiceValue
+    let title = 'THE HOUSE PICKED'
 
-    data = computerChoiceValue
 
     const makeWait = () => {
         const computerChoice = document.createElement('div');
+        computerChoice.classList.add('choice')
         computerChoice.innerHTML = yourChoiceTemplate(data);
 
-        const computerChoiceDisplay = document.querySelector('.computer-choice');
+
         computerChoiceDisplay.appendChild(computerChoice);
 
         const blankCircle = document.querySelector('.blank-div');
         blankCircle.classList.add('hidden');
+
+        computerChoiceData = data
+        compareChoices()
     };
 
     setTimeout(makeWait, 1000);
-
-
+    setTimeout(showResult, 1500)
 }
+
+console.log(userChoiceData)
+console.log(computerChoiceData)
+
+
+// This could probably be done with a switch statement...
+function compareChoices() {
+    if (userChoiceData == 'rock' && computerChoiceData == 'rock') {
+        draw()
+    } else if (userChoiceData == 'rock' && computerChoiceData == 'paper') {
+        win()
+    } else if (userChoiceData == 'rock' && computerChoiceData == 'scissors') {
+        lost()
+    } else if (userChoiceData == 'paper' && computerChoiceData == 'paper') {
+        draw()
+    } else if (userChoiceData == 'paper' && computerChoiceData == 'scissors') {
+        lost()
+    } else if (userChoiceData == 'paper' && computerChoiceData == 'rock') {
+        win()
+    } else if (userChoiceData == 'scissors' && computerChoiceData == 'scissors') {
+        draw()
+    } else if (userChoiceData == 'scissors' && computerChoiceData == 'paper') {
+        win()
+    } else if (userChoiceData == 'scissors' && computerChoiceData == 'rock') {
+        lost()
+    }
+}
+
+function win() {
+    resultText.textContent = 'YOU WIN'
+    counter++
+}
+function lost() {
+    resultText.textContent = 'YOU LOSE'
+    counter--
+}
+function draw() {
+    resultText.textContent = 'DRAW'
+}
+
+
+function showResult() {
+    finalResultDisplay.classList.remove('hidden')
+    console.log(userChoiceData, computerChoiceData)
+    // updateCounter()
+}
+
+//play again
+playAgainBtn.addEventListener('click', () => {
+    finalResultDisplay.classList.add('hidden')
+    resultWaitDisplay.classList.add('hidden')
+    gameDisplay.classList.remove('hidden')
+    while (yourChoice.firstChild) {
+        yourChoice.removeChild(yourChoice.firstChild);
+    }
+
+    // Remove all children from computerChoiceDisplay
+    while (computerChoiceDisplay.firstChild) {
+        computerChoiceDisplay.removeChild(computerChoiceDisplay.firstChild);
+    }
+})
